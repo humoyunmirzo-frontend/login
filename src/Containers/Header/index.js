cf4691783b5c8521a826ed1ecc28d58f7b456e7e
Humoyunmirzo Xoliqberdiyev <111569832+humoyunmirzo-frontend@users.noreply.github.com>
2024-01-07 21:11:03 +0500

    Update index.js

--- src/Containers/Header/index.js
+++ src/Containers/Header/index.js
@@ -4,6 +4,11 @@ import { Link } from 'react-router-dom'
 
 export default function Header() {
     const token = localStorage.getItem('access_token')
+      const navigate = useNavigate()
+  const logout = ()=>{
+    localStorage.removeItem('access_token')
+    navigate('/login')
+  }
     return (
         <header className='flex items-center justify-between px-10 py-5 text-white bg-primary'>
             <Slide direction='left' triggerOnce >
@@ -13,7 +18,9 @@ export default function Header() {
             </Slide>
          
             <Slide direction='right' triggerOnce>
-            Admin ismi
+            <button onClick={()=> logout()} className="px-3 py-1 w-full rounded-md bg-danger text-[#fff]">
+      Chiqish
+    </button>
             </Slide>
         </header>
     )
