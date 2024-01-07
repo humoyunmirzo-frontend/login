25f6bc19d7ea413dbb7d895c880d8f012d19a9d3
Humoyunmirzo Xoliqberdiyev <111569832+humoyunmirzo-frontend@users.noreply.github.com>
2024-01-07 21:10:23 +0500

    Update index.js

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,17 +3,9 @@ import Header from '../../Containers/Header'
 import { useNavigate } from 'react-router-dom'
 
 export default function Home() {
-  const navigate = useNavigate()
-  const logout = ()=>{
-    localStorage.removeItem('access_token')
-    navigate('/login')
-  }
   return (
     <>
     <Header/>
-    <button onClick={()=> logout()} className="px-3 py-1 w-full rounded-md bg-danger text-[#fff]">
-      Chiqish
-    </button>
     </>
   )
 }
